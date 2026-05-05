e39cfa2a0c3cb7a81935da6bbb14bc14dbaa376d
giannasaurus <gianzcantor@gmail.com>
2026-05-05 21:37:54 +0800

    feat: Dropzone
    * created a simple dropzone component (without actual drag & drop
      functionality 💀)
    * added simple styling to dropzone
    * added local Inter font
    * clicking `Replace File` uses `handleReplaceFileClick` to redirect the
      action to a hidden input using the built-in `useRef`
    
    Next component: Header
    Author: Giannasaurus

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Dropzone from './components/dropzone.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
-    <h1>yetToBeNamed</h1>
+    <Dropzone/>
   )
 }
 
---
+++ frontend/src/components/dropzone.jsx
@@ -0,0 +1,29 @@
+// import { useState } from 'react'
+import { useRef } from 'react'
+import uploadIcon from '../assets/icons/upload.svg'
+
+export default function Dropzone() {
+    const replaceFileInputRef = useRef(null)
+
+    function handleReplaceFileClick() {
+        replaceFileInputRef.current.click()
+    }
+
+    return (
+        <main>
+            <div id="dropzone">
+                <div id="dropzone__area">
+                    <img id="uploadIcon" src={uploadIcon} alt="Upload icon" />
+                    <p>Drag & drop to upload</p>
+                    <input id="uploadFileBtn" className="invisible" type="file" />
+                    <label id="uploadFileLabel" htmlFor="uploadFileBtn">Choose File</label>
+                </div>
+                <footer id="dropzone__btnGroup">
+                    <button className="replace-file-btn" type="button" onClick={handleReplaceFileClick}>Replace File</button>
+                    <input ref={replaceFileInputRef} className="invisible" type="file" />
+                    <button className="upload-analyze-btn" type="submit">Upload & Analyze</button>
+                </footer>
+            </div>
+        </main>
+    )
+}

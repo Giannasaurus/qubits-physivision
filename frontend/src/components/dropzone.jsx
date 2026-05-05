e984ade6eded78df987a05bf9755c9f8bed18dfe
giannasaurus <gianzcantor@gmail.com>
2026-05-06 00:57:12 +0800

    feat(dropzone): Make it so the entire dropzone area is clickable and opens the file system (#2)

--- frontend/src/components/dropzone.jsx
+++ frontend/src/components/dropzone.jsx
@@ -12,12 +12,14 @@ export default function Dropzone() {
     return (
         <main>
             <div id="dropzone">
-                <div id="dropzone__area">
-                    <img id="uploadIcon" src={uploadIcon} alt="Upload icon" />
-                    <p>Drag & drop to upload</p>
-                    <input id="uploadFileBtn" className="invisible" type="file" />
-                    <label id="uploadFileLabel" htmlFor="uploadFileBtn">Choose File</label>
-                </div>
+                <label id="dropzone__clickable" htmlFor="uploadFileBtn">  
+                    <div id="dropzone__area">
+                        <img id="uploadIcon" src={uploadIcon} alt="Upload icon" />
+                        <p>Drag & drop to upload</p>
+                        <input id="uploadFileBtn" className="invisible" type="file" />
+                        <label id="uploadFileLabel" htmlFor="uploadFileBtn">Choose File</label>
+                    </div>
+                </label>
                 <footer id="dropzone__btnGroup">
                     <button id="replace-file-btn" type="button" onClick={handleReplaceFileClick}>Replace File</button>
                     <input ref={replaceFileInputRef} className="invisible" type="file" />

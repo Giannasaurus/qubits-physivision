8aebb616f2ae191c143050027e152999a4c11637
giannasaurus <gianzcantor@gmail.com>
2026-05-06 10:17:05 +0800

    feat(dropzone): Added video preview (after-upload) (#3)
    * conditional components, where:
      * videoPreview appears only when a videoUrl exists
      * dropzone clickable is the default first child component of dropzone
    when there's no videoUrl, otherwise videoPreview
    * upload and replace buttons trigger previewSelectedVideo to return
      videoPreview
    * style video element to scale proportionally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Dropzone from './components/dropzone.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   return (
     <>
       <Header/>
--- frontend/src/components/dropzone.jsx
+++ frontend/src/components/dropzone.jsx
@@ -1,31 +1,81 @@
-// import { useState } from 'react'
-import { useRef } from 'react'
-import uploadIcon from '../assets/icons/upload.svg'
+import { useEffect, useRef, useState } from "react";
+import uploadIcon from "../assets/icons/upload.svg";
+
+function VideoPreview({ videoUrl }) {
+  return <video className="video-preview" controls src={videoUrl} autoplay muted />;
+}
 
 export default function Dropzone() {
-    const replaceFileInputRef = useRef(null)
+  const [videoUrl, setVideoUrl] = useState(null);
+  const replaceFileInputRef = useRef(null);
 
-    function handleReplaceFileClick() {
-        replaceFileInputRef.current.click()
+  function handleReplaceFileClick() {
+    replaceFileInputRef.current.click();
+  }
+
+  function previewSelectedVideo(e) {
+    const video = e.target.files[0];
+
+    if (video) {
+      const url = URL.createObjectURL(video);
+      setVideoUrl(url);
     }
+  }
 
-    return (
-        <main>
-            <div id="dropzone">
-                <label id="dropzone__clickable" htmlFor="uploadFileBtn">  
-                    <div id="dropzone__area">
-                        <img id="uploadIcon" src={uploadIcon} alt="Upload icon" />
-                        <p>Drag & drop to upload</p>
-                        <input id="uploadFileBtn" className="invisible" type="file" accept="video/*" />
-                        <label id="uploadFileLabel" htmlFor="uploadFileBtn">Choose File</label>
-                    </div>
-                </label>
-                <footer id="dropzone__btnGroup">
-                    <button id="replace-file-btn" type="button" onClick={handleReplaceFileClick}>Replace File</button>
-                    <input ref={replaceFileInputRef} className="invisible" type="file" accept="video/*" />
-                    <button id="upload-analyze-btn" type="submit">Upload & Analyze</button>
-                </footer>
+  useEffect(() => {
+    return () => {
+      if (videoUrl) {
+        URL.revokeObjectURL(videoUrl);
+      }
+    };
+  }, [videoUrl]);
+
+  return (
+    <main>
+      <div id="dropzone">
+        {videoUrl && <VideoPreview videoUrl={videoUrl} />}
+
+        {!videoUrl && (
+          <label id="dropzone__clickable" htmlFor="uploadFileBtn">
+            <div id="dropzone__area">
+              <img id="uploadIcon" src={uploadIcon} alt="Upload icon" />
+              <p>Drag & drop to upload</p>
+              <input
+                id="uploadFileBtn"
+                className="invisible"
+                type="file"
+                accept="video/*"
+                onChange={previewSelectedVideo}
+              />
+              <label id="uploadFileLabel" htmlFor="uploadFileBtn">
+                Choose File
+              </label>
             </div>
-        </main>
-    )
+          </label>
+        )}
+
+        {videoUrl && (
+          <footer id="dropzone__btnGroup">
+            <button
+              id="replace-file-btn"
+              type="button"
+              onClick={handleReplaceFileClick}
+            >
+              Replace File
+            </button>
+            <input
+              ref={replaceFileInputRef}
+              className="invisible"
+              type="file"
+              accept="video/*"
+              onChange={previewSelectedVideo}
+            />
+            <button id="upload-analyze-btn" type="submit">
+              Upload & Analyze
+            </button>
+          </footer>
+        )}
+      </div>
+    </main>
+  );
 }
